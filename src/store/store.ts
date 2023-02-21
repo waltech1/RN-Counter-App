486ec580b7069ede998f69b5b45d195cf3e435b2
waltech1 <walTech@protonmail.com>
2023-02-21 13:32:15 -0600

    feat: implement redux

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlice';
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
